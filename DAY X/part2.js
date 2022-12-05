7844989bd632cad513fcf65dcf477708b3defe63
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2022-12-05 18:57:18 +0500

    base setup

---
+++ DAY X/part2.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const readline = require("readline");
+
+// Open the file as a readable stream
+const fileStream = fs.createReadStream("./in");
+
+// Create a new readline interface with the crlfDelay option set to Infinity
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity,
+});
+
+rl.on("line", (line) => {
+  // each line is a round.
+});
+
+// Handle any errors
+rl.on("error", (err) => {
+  console.error(err);
+});
+
+// Close the stream when the file has been read
+rl.on("close", () => {});
