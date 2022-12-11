3d28aa3645694519edaccd814fb93555dd5a02ab
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2022-12-11 17:59:24 +0500

    day4 part1 very imperative solution 💩

--- Day 4/part1.js
+++ Day 4/part1.js
@@ -10,8 +10,24 @@ const rl = readline.createInterface({
   crlfDelay: Infinity,
 });
 
+let totalContainments = 0;
 rl.on("line", (line) => {
-  // each line is a round.
+  // each line is a pair.
+  const [rawRange1, rawRange2] = line.split(",");
+  const [start, end] = rawRange1.split("-");
+  const [start2, end2] = rawRange2.split("-");
+  const parsedRanges = [
+    { start: +start, end: +end },
+    { start: +start2, end: +end2 },
+  ];
+  if (
+    (parsedRanges[0].start <= parsedRanges[1].start &&
+      parsedRanges[0].end >= parsedRanges[1].end) ||
+    (parsedRanges[1].start <= parsedRanges[0].start &&
+      parsedRanges[1].end >= parsedRanges[0].end)
+  ) {
+    totalContainments += 1;
+  }
 });
 
 // Handle any errors
@@ -20,4 +36,6 @@ rl.on("error", (err) => {
 });
 
 // Close the stream when the file has been read
-rl.on("close", () => {});
+rl.on("close", () => {
+  console.log({ totalContainments });
+});
